8583c23b8bf2fe1962dd0bb0454d971ed8e32578
Ilya Sevrin <i.sevrin@outlook.com>
2023-04-04 18:58:12 +0300

    delete garbage

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
 
-        String name = new String("Zaur");
-        System.out.println(name);
     }
 }
